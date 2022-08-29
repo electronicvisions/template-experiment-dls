103aa074b4fa54a54d9119664cd7a69254b6bd24
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2022-08-29 12:33:35 +0200

    Fix python tests to be runnable as scripts
    * did not run beforehand because the test framework was never
      started only defined
    
    Change-Id: I68049edd04f26a611a6ac19a2ea03fc25f808672

--- tests/hw/py/template/test_minimal_experiment.py
+++ tests/hw/py/template/test_minimal_experiment.py
@@ -6,3 +6,7 @@ from template.scripts.minimal_experiment import main
 class TestMinimalExperiment(unittest.TestCase):
     def test_experiment(self):
         self.assertIsNone(main())
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/sw/py/template/test_adder.py
+++ tests/sw/py/template/test_adder.py
@@ -6,3 +6,7 @@ from template import add
 class TestAdder(unittest.TestCase):
     def test_commutative(self):
         self.assertEqual(add(3, 4), add(4, 3))
+
+
+if __name__ == "__main__":
+    unittest.main()
